8364417251781080e110c731a719765db0a7a212
Ezequiel Novoa <ezequiel.novoa3@gmail.com>
2023-03-22 02:46:12 -0700

    Create a colors file to organize my code.

---
+++ app/config/colors.js
@@ -0,0 +1,11 @@
+export default {
+  primary: "#4d80bd",
+  secondary: "#504dbd",
+  tertiary: "red",
+  black: "#000",
+  white: "#fff",
+  grey: "#565f71ff",
+  midBlue: "#4d82bd",
+  purple: "#3f2844ff",
+  gold: "#ba1a",
+};
